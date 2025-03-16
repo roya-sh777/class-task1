92039d4d2d169026c16f8c2d2458683ec8d63230
Roya <royash-bb104@code.edu.az>
2025-03-16 03:24:05 -0700

    task2

--- Program.cs
+++ Program.cs
@@ -1,52 +1,44 @@
-﻿ namespace ConsoleApp2
- {
-        internal class Program
+﻿using System.Runtime.InteropServices;
+
+namespace ConsoleApp1
+{
+    internal class Student
+
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Group { get; set; }
+        public int Point { get; set; }
+        public bool isGraduated { get; set; }
+        public Student(string name, string surname, string group, int point)
         {
-            class Student
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+            isGraduated = point >= 65;
+        }
+        public void Getinfo()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Surname: {Surname}");
+            Console.WriteLine($"Group: {Group}");
+            Console.WriteLine($"Point: {Point}");
+            Console.WriteLine($"IsGraduated: {(isGraduated ? "Yes" : "No")}");
+        }
+        class Program
+        {
+            static void Main()
             {
-                public string Name { get; set; }
-                public string Surname { get; set; }
-                public string Group { get; set; }
-                public int Point { get; set; }
-                public bool IsGraduated { get; set; }
-                public Student(string name, string surname, string group, int point)
-                {
-                    Name = name;
-                    Surname = surname;
-                    Group = group;
-                    Point = point;
-                    IsGraduated = point >= 65;
-                }
-                public void Getinfo()
-                {
-                    Console.WriteLine($"Name: {Name}");
-                    Console.WriteLine($"Surname: {Surname}");
-                    Console.WriteLine($"Group: {Group}");
-                    Console.WriteLine($"Point: {Point}");
-                    Console.WriteLine($"IsGraduated: {(IsGraduated ? "Yes" : "No")}");
-                }
-                public void CheckGraduated()
-                {
-                if (IsGraduated)
-                    Console.WriteLine($"{Name} {Surname} mezun olub.");
-                else
-                    Console.WriteLine($"{Name} {Surname} mezun olmayib");
-                }
-                class Program
-                {
-                    static void Main()
-                    {
-                        Student student1 = new Student("Roya", "Huseynova", "R25", 75);
-                        student1.Getinfo();
-                        Console.WriteLine();
-                        Student student2 = new Student("Asiman", "Aliyev", "R25", 55);
-                        student2.Getinfo();
-                    }
-                }
+                Student student1 = new Student("Roya", "Huseynova", "R25", 75);
+                student1.Getinfo();
+                Console.WriteLine();
+                Student student2 = new Student("Asiman", "Aliyev", "R25", 55);
+                student2.Getinfo();
             }
         }
- }
-
+    }
+}
 
 
 
